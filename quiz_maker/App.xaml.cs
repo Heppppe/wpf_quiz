935b410e85363cfacc015545af28cb3c3b784cb9
Heppe Heppe <mateusz.chlopecki1@gmail.com>
2025-04-30 16:47:59 +0200

    empty projects

---
+++ quiz_maker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace quiz_maker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
